ef0aed66314e330d1981f500b8bc084472777c59
TianChaoFanRobot <714857525@qq.com>
2020-10-30 10:53:54 +0800

    tcf1030

--- src/main/java/com/tcf/kid/smart/framework/model/MapperBean.java
+++ src/main/java/com/tcf/kid/smart/framework/model/MapperBean.java
@@ -1,6 +1,7 @@
 package com.tcf.kid.smart.framework.model;
 
 import java.util.List;
+import java.util.Map;
 
 /***
  * TODO TCF 封装映射器接口信息
@@ -14,6 +15,9 @@ public class MapperBean {
 	
 	//TODO TCF 映射器接口中定义的所有持久化方法
 	private List<Function> functions;
+
+	// TODO TCF 这是一个钩子
+	private Map<String,Object> hookMap;
 	
 	public String getInterfaceName() {
 		return interfaceName;
@@ -27,5 +31,12 @@ public class MapperBean {
 	public void setFunctions(List<Function> functions) {
 		this.functions = functions;
 	}
-	
+
+	public Map<String, Object> getHookMap() {
+		return hookMap;
+	}
+
+	public void setHookMap(Map<String, Object> hookMap) {
+		this.hookMap = hookMap;
+	}
 }
